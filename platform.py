d303b4bd5eb806e43c475d9d332cb6939a3bdc33
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-09-24 17:19:35 +0200

    Rename setup function to _setup_pipenv_minimal

--- platform.py
+++ platform.py
@@ -848,7 +848,7 @@ class Espressif32Platform(PlatformBase):
             core_dir = config.get("platformio", "core_dir")
             
             # Setup penv using minimal function (no SCons dependencies, esptool from tl-install)
-            penv_python, esptool_path = setup_penv_minimal(self, core_dir, install_esptool=True)
+            penv_python, esptool_path = _setup_pipenv_minimal(self, core_dir, install_esptool=True)
             
             # Store both for later use
             self._penv_python = penv_python
